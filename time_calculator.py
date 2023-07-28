9c4ae005874683ee03a2e0439d9be8e74f9a4713
redrootoct <60930962+redrootoct@users.noreply.github.com>
2023-07-28 12:15:33 +0300

    Update time_calculator.py

--- time_calculator.py
+++ time_calculator.py
@@ -1,130 +1,87 @@
-def add_time(input1, input2, useWeekdays = False):
+def add_time(start, duration, dw=None):
+    my_str1 = start.split()
+    my_str2 = my_str1[0].split(":")
+    my_str3 = duration.split(":")
 
-    #Process inputs
-    ###############
+    new_time=""
+    ampm=0
+    a1=None
+    a2=None
+    a3=None
+    a4=None
 
-    whole_current_time = input1.split()
-
-    current_time = whole_current_time[0].split(":")
-
-    current_time_of_day = whole_current_time[1]
-
-    if current_time_of_day == "PM":
-        if current_time[0] != "12": #if hour is not 12, sum 12 to hour, otherwise leave hour as it is
-            int_current_time_hour = int(current_time[0])
-            int_current_time_hour += 12
-            current_time[0] = str(int_current_time_hour)
-
-    if current_time_of_day == "AM" and current_time[0] == "12": #if it is 12AM, convert it to 0
-        current_time[0] = "00"
-
-    time_to_add = input2.split(":")
-
-    #Do calculation
-    ###############
-
-    #Calculate minutes
-
-    minutes_sum = int(time_to_add[1]) + int(current_time[1])
-
-    if minutes_sum >= 60:
-        minute_diff = minutes_sum - 60
-        minutes = minute_diff
-        hours_to_add = 1
+    #am/pm trigger
+    if my_str1[1]=="AM":
+        ampm=1
     else:
-        minutes = minutes_sum
-        hours_to_add = 0
+        ampm=2
 
-    #Calculate hour
 
-    hour_sum = int(time_to_add[0]) + int(current_time[0]) + hours_to_add
-    if hour_sum >= 24:
-        extra_days_int = hour_sum // 24
-        hour = hour_sum - 24 * extra_days_int
+    #sum h
+    sum1=(int(my_str2[0])+int(my_str3[0]))
+    h1=sum1//12
+    h2=sum1%12
+
+    #sum min
+    sum2=(int(my_str2[1])+int(my_str3[1]))
+    m1=sum2//60
+    m2=sum2%60
+
+    #min
+    if sum2>60:
+        h2+=1
+            
+    if (sum1+m1)//12==0:
+        a1=str(sum1) + ":"
+        if m1==0:
+            a1+=str(sum2).rjust(2,"0")
+
+    #apmp
+    ampm+=(sum1+m1)//12
+    apmp2=ampm % 2 
+    if ampm % 2 ==0:
+        a2="PM"
     else:
-        hour = hour_sum
-        extra_days_int = 0
+        a2="AM"
 
-    #Calculate weekdays
-
-    if useWeekdays != False:
-        useWeekdays = useWeekdays.lower()
-        if useWeekdays == "monday":
-            start_weekday_number = 1
-        elif useWeekdays == "tuesday":
-            start_weekday_number = 2
-        elif useWeekdays == "wednesday":
-            start_weekday_number = 3
-        elif useWeekdays == "thursday":
-            start_weekday_number = 4
-        elif useWeekdays == "friday":
-            start_weekday_number = 5
-        elif useWeekdays == "saturday":
-            start_weekday_number = 6
-        elif useWeekdays == "sunday":
-            start_weekday_number = 7
-
-        weekday_number = (start_weekday_number + extra_days_int) % 7
-
-        if weekday_number == 1:
-            weekday = "Monday"
-        elif weekday_number == 2:
-            weekday = "Tuesday"
-        elif weekday_number == 3:
-            weekday = "Wednesday"
-        elif weekday_number == 4:
-            weekday = "Thursday"
-        elif weekday_number == 5:
-            weekday = "Friday"
-        elif weekday_number == 6:
-            weekday = "Saturday"
-        elif weekday_number == 0:
-            weekday = "Sunday"
-
-    #Formatting time to string
-
-    def timeNumberFormat(n) :
-        if n < 10:
-            return "0{}".format(n)
+    if (sum1+m1)//12>0:
+        a1=str(h2) + ":"
+        if m1==0:
+            a1+=str(sum2)
         else:
-            return "{}".format(n)
+            a1+=str(m2).rjust(2,"0")
 
-    if hour >= 12:
-        rest = 24 - hour
-        hour = 12 - rest
-        time_of_day = "PM"
+    #days later
+    if ampm>2:
+        laterday = ampm//2
+        if laterday==1:
+            a4="(next day)"
+        if laterday>=2:
+            a4="("+ str(laterday) + " days later)"
 
-        if hour == 12: #if time is 24 then it should be 12 AM
-            time_of_day = "AM"
-        if hour == 0:  #if hour is 12 then it should be 12 PM
-            hour = 12
 
-    else:
-        time_of_day= "AM"
-        if hour == 0: #if hour is 00 then it should be 12 AM
-            hour = 12
+    #get number
+    if dw!= None:
 
-    formatted_hour = str(hour)
-    formatted_minutes = timeNumberFormat(minutes)
-    formatted_extra_days = str(extra_days_int)
+        dayofweek=["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
+        input_dayofweek = dw.lower()
 
-    #print
-
-    if extra_days_int == 0:
-        if useWeekdays == False:
-            new_time = formatted_hour+":"+formatted_minutes+" "+time_of_day
+        i=dayofweek.index(input_dayofweek)
+        i_plues = ampm//2
+        if ampm >2: 
+            i=i+i_plues
+            ii=i%7
+            a3=dayofweek[ii]
         else:
-            new_time = formatted_hour + ":" + formatted_minutes + " " + time_of_day+", "+weekday
-    else:
-        if useWeekdays == False:
-          if extra_days_int == 1:
-            new_time = formatted_hour+":"+formatted_minutes+" "+time_of_day+" (next day)"
-          else:
-            new_time = formatted_hour+":"+formatted_minutes+" "+time_of_day+" ("+formatted_extra_days+" days later)"
-        else:
-          if extra_days_int == 1:
-             new_time = formatted_hour + ":" + formatted_minutes + " " + time_of_day + ", " + weekday+ " (next day)"
-          else:
-             new_time = formatted_hour + ":" + formatted_minutes + " " + time_of_day + ", " + weekday+ " (" +formatted_extra_days + " days later)"
+            a3=input_dayofweek
+
+    if a3==None and a4==None:
+        new_time = a1+" "+a2
+    elif  a4==None:
+        new_time = a1+" "+a2+", "+a3.capitalize()
+    elif  a3==None:
+        new_time = a1+" "+a2+" "+a4
+    else:        
+        new_time = a1+" "+a2+", "+a3.capitalize()+" "+a4
 
     return new_time
